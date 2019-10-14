ab1cbcb6dda9e40ee353239adcc321acf77078d4
Andre Guimaraes <andreguimaraes@tokenlab.com.br>
2019-10-14 16:07:58 -0300

    Fix PR requests

--- src/app/inputs/input-select-chunk/input-select-chunk.component.ts
+++ src/app/inputs/input-select-chunk/input-select-chunk.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractInput } from '../abstract-input/abastract-input.component';
-import { NgForm } from '@angular/forms';
 
 export interface Chunk {
     phoneDigits: string;
@@ -12,51 +11,32 @@ export interface Chunk {
     styleUrls: ['./input-select-chunk.component.scss']
 })
 export class InputSelectChunkComponent extends AbstractInput {
-    
-    firstChunkValue: string; 
+
+    firstChunkValue: string;
     secondChunkValue: string;
     thirdChunkValue: string;
     chunks: Chunk[] = [];
 
     constructor() {
         super();
-        let phoneDigits = 0;
-        while (phoneDigits < 10) {
-            this.chunks.push({phoneDigits: "00"+phoneDigits});
-            phoneDigits++;
-        }
-        while (phoneDigits < 100) {
-            this.chunks.push({phoneDigits: "0"+phoneDigits});
-            phoneDigits++;
-        }
-        while (phoneDigits < 1000) {
-            this.chunks.push({phoneDigits: ""+phoneDigits});
-            phoneDigits++;
-        }
-        shuffleArray(this.chunks);
+        this.chunks = Array(10000).fill(0).map((_, i) => ('000' + i).substr(-4)).map(result => ({phoneDigits: result}));
+        this.chunks = shuffleArray(this.chunks);
     }
 
 
     concatenateDigits() {
-        this.value = +(this.firstChunkValue+this.secondChunkValue+this.thirdChunkValue);
+        this.value = +(this.firstChunkValue + this.secondChunkValue + this.thirdChunkValue);
         this.submit();
     }
 
-    resetForm(){
-        this.firstChunkValue = "0000"
-        this.secondChunkValue = "0000"
-        this.thirdChunkValue = "0000"
+    resetForm() {
+        this.firstChunkValue = '0000';
+        this.secondChunkValue = '0000';
+        this.thirdChunkValue = '0000';
         this.reset();
     }
-
-    ngOnInit() {
-    }
-
 }
 
-function shuffleArray(array : Chunk[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+function shuffleArray<T>(array: T[]): T[] {
+    return array.sort(() => 0.5 - Math.random());
 }
